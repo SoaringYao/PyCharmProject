463ac91c26d720431d5c5411df5d0ce259e8c89d
Saturnus001 <David241025@outlook.com>
2024-02-05 16:11:10 +0800

    refresh

--- Python Core Data Types/list.py
+++ Python Core Data Types/list.py
@@ -3,17 +3,21 @@ print(f'{len(L)}\n')
 
 L.append('NI')
 print(f'{L}\n')
+# [123, 'spam', 1.23, 'NI']
 
 L.insert(1, 'eggs')
 print(f'{L}\n')
+# [123, 'eggs', 'spam', 1.23, 'NI']
 
 print(L.pop())
+# NI
 print(f'{L}\n')
+# [123, 'eggs', 'spam', 1.23]
 
 print(L.pop(1))
+# eggs
 print(f'{L}\n')
+# [123, 'spam', 1.23]
 
 L.remove('spam')
-print(f'{L}\n')
-
-
+print(f'{L}\n')  # [123, 1.23]
